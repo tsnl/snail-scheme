4700a8d396b2c6b76643fdaaccf3053b10f2c6e0
Nikhil Tony Idiculla <nikhilidiculla@gmail.com>
2022-12-27 20:18:28 -0800

    wip

--- sandbox/demo13.scm
+++ sandbox/demo13.scm
@@ -1,12 +1,7 @@
 ; (define x 0)
 ; (define y x)
-(define factorial (lambda (n) (if (p/invoke = n 0) 1 (p/invoke * n (p/invoke - n 1)))))
-(define sumtorial (lambda (n) (if (p/invoke = n 0) 1 (p/invoke + n (p/invoke - n 1)))))
+(define factorial (lambda (n) (if (p/invoke = n 0) 1 (p/invoke * n (factorial (p/invoke - n 1))))))
+(define sumtorial (lambda (n) (if (p/invoke = n 0) 1 (p/invoke + n (sumtorial (p/invoke - n 1))))))
 
-(p/invoke displayln (factorial 0))
-(p/invoke displayln (factorial 1))
-(p/invoke displayln (factorial 2))
+(p/invoke displayln (factorial 15))
 
-(p/invoke displayln (sumtorial 0))
-(p/invoke displayln (sumtorial 1))
-(p/invoke displayln (sumtorial 2))
--- sandbox/demo3.scm
+++ sandbox/demo3.scm
@@ -18,7 +18,7 @@
 (define a0 (fibonacci 2))
 ; (p/invoke displayln a0)
 
-(define a1 (fibonacci 30))
+; (define a1 (fibonacci 10))
 ; (display (format "~a\n" a1))
 ; (define a2 (fibonacci 30))
 ; (display (format "~a\n" a2))
@@ -36,4 +36,4 @@
 ;     (x a)))
 ; (call/cc w)
 
-; (p/invoke displayln a1)
+(p/invoke displayln a0)
